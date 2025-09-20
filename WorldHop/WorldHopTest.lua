69700b1426e5960a9edf5c26b6db21442cda2de7
m-qq <mads.qq@mail.com>
2025-09-19 20:38:11 -0400

    Delete WorldHop directory

--- WorldHop/WorldHopTest.lua
+++
@@ -1,7 +0,0 @@
-local API         = require("api")
-local WorldHop	  = require("WorldHop")
-
-while API.Read_LoopyLoop() do
-	WorldHop.checkHopWorlds()
-	API.RandomSleep2(math.random(600, 1800), 1200, 600)
-end
